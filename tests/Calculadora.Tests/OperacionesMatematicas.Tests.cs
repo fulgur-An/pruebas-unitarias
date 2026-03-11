d709baa1ece59900fa035deda7954576e46df660
DOMINGUEZ GARCIA ANTONIO DE JESUS <zS19014049@estudiantes.uv.mx>
2026-03-11 14:40:39 -0600

    adding tests

--- tests/Calculadora.Tests/OperacionesMatematicas.Tests.cs
+++ tests/Calculadora.Tests/OperacionesMatematicas.Tests.cs
@@ -48,4 +48,50 @@ public class OperacionesMatematicasTest
       // Assert
       Assert.Equal(0, resultado);
   }
+
+  //pruebas de restas
+  [Fact]
+  public void Restar_DosNumeros_RetornaResta()
+  {
+      // Arrange
+      var operaciones = new OperacionesMatematicas();
+      double a = 5;
+      double b = 3;
+
+      // Act
+      double resultado = operaciones.Restar(a, b);
+
+      // Assert
+      Assert.Equal(2, resultado);
+  }
+
+  [Fact]
+  public void Restar_NegativoMenosPositivo_RetornaValorCorrecto()
+  {
+      // Arrange
+      var operaciones = new OperacionesMatematicas();
+      double a = -5;
+      double b = 3;
+
+      // Act
+      double resultado = operaciones.Restar(a, b);
+
+      // Assert
+      Assert.Equal(-8, resultado);
+  }
+
+  [Fact]
+  public void Restar_CeroMenosCero_RetornaCero()
+  {
+      // Arrange
+      var operaciones = new OperacionesMatematicas();
+      double a = 0;
+      double b = 0;
+
+      // Act
+      double resultado = operaciones.Restar(a, b);
+
+      // Assert
+      Assert.Equal(0, resultado);
+  }
 }
